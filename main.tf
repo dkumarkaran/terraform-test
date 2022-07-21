68743e41d3b61d6d44e955de595008ccfefe6328
Kumar_Karan <102618890+dkumarkaran@users.noreply.github.com>
2022-07-21 18:28:58 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "google" {
+  project     = "new-project-356711"
+  region      = "us-central1"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
